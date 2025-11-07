c1cc433528e068dea47889aa70737529a32e1f3f
myakotnikovmikhail <myakotnikov.m@mail.ru>
2025-11-07 12:11:44 +0500

    add comment to courses

--- models/course.go
+++ models/course.go
@@ -1,5 +1,7 @@
+// Package models provides structures of database tables
 package models
 
+// Course represents a course with a ID, Title, TeacherID and TeacherName.
 type Course struct {
 	ID          int
 	Title       string
