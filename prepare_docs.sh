3f3d8397c3037c7f411f7f073bc864d20fe7a4ee
myakotnikovmikhail <myakotnikov.m@mail.ru>
2026-04-02 12:28:55 +0500

    fix ci

--- prepare_docs.sh
+++ prepare_docs.sh
@@ -9,24 +9,25 @@ mkdir -p public
 cp -r godoc/* public/
 
 # move main page to the root
-cp public/pkg/my-app-go/index.html public/index.html
+cp public/pkg/$PROJECT_NAME/index.html public/index.html
 
 # remove localhost (replacing it with project root)
-find public -name "*.html" -exec sed -i 's|http://localhost:6060/pkg/|/my-app-go/pkg/my-app-go/|g' {} +
-find public -name "*.html" -exec sed -i 's|http://localhost:6060/|/my-app-go/|g' {} +
+find public -name "*.html" -exec sed -i "s|http://localhost:6060/pkg/|/$PROJECT_NAME/pkg/$PROJECT_NAME/|g" {} +
+find public -name "*.html" -exec sed -i "s|http://localhost:6060/|/$PROJECT_NAME/|g" {} +
 
 # make all links absolute relative to the domain
 # path in file system: ../../lib/godoc/style.css
 # have to become URL: /my-app-go/lib/godoc/style.css
-find public -name "*.html" -exec sed -i 's|\.\./|/my-app-go/|g' {} +
-
+find public -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" \) -exec sed -i "s|\.\./|/$PROJECT_NAME/|g" {} 
 # cleanup possible duplicates
-find public -name "*.html" -exec sed -i 's|//my-app-go/|/my-app-go/|g' {} +
+find public -type f \( -name "*.html" -o -name "*.css" -o -name "*.js" \) -exec sed -i "s|//$PROJECT_NAME/|/|g" {} +
+# one more cleanup. path must start with /my-app-go/
+find public -type f \( -name "*.html" -o -name "*.css" -o -name
 
 # fixing links to pkg files (handlers.html -> pkg/my-app-go/handlers.html)
 # it's needed for index.html in the root
-sed -i 's|href="handlers.html"|href="pkg/my-app-go/handlers.html"|g' public/index.html
-sed -i 's|href="models.html"|href="pkg/my-app-go/models.html"|g' public/index.html
-sed -i 's|href="repositories.html"|href="pkg/my-app-go/repositories.html"|g' public/index.html
+sed -i "s|href=\"handlers.html\"|href=\"pkg/$PROJECT_NAME/handlers.html\"|g" public/index.html
+sed -i "s|href=\"models.html\"|href=\"pkg/$PROJECT_NAME/models.html\"|g" public/index.html
+sed -i "s|href=\"repositories.html\"|href=\"pkg/$PROJECT_NAME/repositories.html\"|g" public/index.html
 
 echo "Documentation is ready"
